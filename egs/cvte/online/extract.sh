062e741d0a188cdbbf001969e9e698f249bfb8b2
aaa-co <draculagj@aliyun.com>
2020-05-22 14:38:12 +0800

    Update extract.sh

--- egs/cvte/online/extract.sh
+++ egs/cvte/online/extract.sh
@@ -2,10 +2,13 @@
 
 # list=`ls data/wav`
 list=`find data/wav -iname "*.wav"`
+echo '' > result.txt
 
 for dir in $list; do
 (
     wav=`basename $dir .wav`
-    grep ^$wav log/* > result.txt
+    grep ^$wav log/* >> result.txt
 )
 done 
+
+cat result.txt
