b0086143005631da8ebcf3b34a40a34abb049ea6
aaa-co <draculagj@aliyun.com>
2020-06-29 18:10:37 +0800

    Update extract.sh

--- egs/cvte/online/extract.sh
+++ egs/cvte/online/extract.sh
@@ -14,4 +14,29 @@ for dir in $list; do
 )
 done
 
+
+groups_array=()
+
+for dir in $list; do
+    wav=`basename $dir .wav`
+    wav_path=`dirname $dir`
+    group_c=$(basename "$wav_path")
+#     echo ${group##*-}
+#     echo ${group%%-*}
+
+    group=${group_c%%-*}
+    if [[ ! "${groups_array[@]}" =~ $group ]] ; then
+        groups_array+=("$group")
+    fi
+    
+#     groups_array[${#groups_array[*]}]=$group
+#     groups_array=("${groups_array[@]}" $group)
+    output=$(basename "$wav_path")-${wav}
+    grep ^${output} log/* >> result/${output}.txt
+done
+
+for group in ${groups_array[*]}; do
+    echo $group
+done
+
 cat result/*.txt
