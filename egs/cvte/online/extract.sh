9e57b6198089ca350a9fcf8544fe337bd22afd04
aaa-co <draculagj@aliyun.com>
2020-06-30 13:38:42 +0800

    Update extract.sh

--- egs/cvte/online/extract.sh
+++ egs/cvte/online/extract.sh
@@ -2,18 +2,11 @@
 
 mv result/* result-bak/
 
+# rm result/*
+# rm log/*.txt
+
 # list=`ls data/wav`
 list=`find data/wav -iname "*.wav"`
-
-for dir in $list; do
-(
-    wav=`basename $dir .wav`
-    wav_path=`dirname $dir`
-    output=$(basename "$wav_path")-${wav}
-    grep ^${output} log/* >> result/${output}.txt
-)
-done
-
 
 groups_array=()
 
@@ -29,14 +22,23 @@ for dir in $list; do
         groups_array+=("$group")
     fi
     
-#     groups_array[${#groups_array[*]}]=$group
-#     groups_array=("${groups_array[@]}" $group)
     output=$(basename "$wav_path")-${wav}
-    grep ^${output} log/* >> result/${output}.txt
+    grep ^${output} log/* >> log/${output}.txt
 done
 
 for group in ${groups_array[*]}; do
-    echo $group
+#     echo $group
+    echo "${group}_a1" | tr -d '\n' > result/${group}_a1.txt
+    group_count=`find log/ -name "${group}*" | wc -l`
+    int=0
+    while(( $int<${group_count} ))
+    do
+        txt=$(cat `find log/ -name "$group-$int-*"`)
+        echo $txt | awk -F ' ' '{$1="";printf $0}' >> result/${group}_a1.txt
+#         echo $txt | awk -F ' ' '{$1="";printf $0}'| tr -d ' ' >> result/${group}_a1.txt
+        let "int++"
+    done
+    rm log/${group}*
 done
 
 cat result/*.txt
